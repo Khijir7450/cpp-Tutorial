db318042ff2e0d292dfb5dcc058c4d573ecabe8b
Khijir7450 <iamkhijiralam@gmail.com>
2023-10-14 11:05:05 +0530

    Add some file

--- loops.cpp
+++ loops.cpp
@@ -3,42 +3,40 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int n = 0;
+    int n = 100;
+    int sum = 0;
+    //  sum of even number
+    while (n <= 200)
+    {
+        sum += n; // sum = sum + n;
+
+        n = n + 2; // n += 2;
+    }
 
     // do
     // {
     //     cout<<n<<" ";
     //     n++;
-        
-        
+
     // }while(n <0);
 
-    cout<<endl;
-    // while loop 
+    // cout<<endl;
+    // // while loop
 
-    while (n<=100)
-    {
-        if(n%2 == 0){
-            cout<< n<< " ";
-        }
-        n++;
-    }
+    // while (n<=100)
+    // {
+    //     if(n%2 == 0){
+    //         cout<< n<< " ";
+    //     }
+    //     n++;
+    // }
     // 0 -- 100
-    
-
 
     // increment operators
     // n++ / n = n+1;
 
-    
-
-
-    
-
     return 0;
 }
-
-
-
